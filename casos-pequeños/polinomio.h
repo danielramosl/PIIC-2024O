be1c8eb5dd35d8529152364565b722b06d1a71b2
Daniel Ramos <91300869+danielramosl@users.noreply.github.com>
2025-01-12 20:00:51 -0600

    Update polinomio.h

--- casos-pequeños/polinomio.h
+++ casos-pequeños/polinomio.h
@@ -1,131 +1,102 @@
-#include "ops128.h"
-#include <string>
+#pragma once
+#include <atomic>
+#include <quadmath.h>
+#include <thread>
+#include <stdfloat>
+#include <utility>
 #include <vector>
+#include "coloración.h"
+#include "estadística.h"
+#include "ops128.h"
 
-class polinomio {
+class monteCarlo {
     private:
-    struct arista {
-        int u;
-        int v;
-    };
-    std::string sPolinomio;
+    std::float128_t z;
+    std::float128_t e;
     std::vector<std::vector<bool>> g;
-    std::vector<int> fun;
-
-    bool no_arista(const std::vector<std::vector<bool>>& mat) {
-        for(int i = 0; i < mat.size(); ++i) {
-            for(int j = i + 1; j < mat.size(); ++j) {
-                if(mat[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    arista primer_arista(const std::vector<std::vector<bool>>& mat) {
-        for(int i = 0; i < mat.size(); ++i) {
-            for(int j = i + 1; j < mat.size(); ++j) {
-                if(mat[i][j] == 1) {
-                    return arista {i, j};
-                }
-            }
-        }
-        return arista {0,0};
-    }
-
-    std::vector<std::vector<bool>> borrado(std::vector<std::vector<bool>> mat, arista arista) {
-        mat[arista.u][arista.v] = 0; mat[arista.v][arista.u] = 0;
-        return mat;
-    }
-
-    std::vector<std::vector<bool>> contracción(const std::vector<std::vector<bool>>& mat, arista arista) {
-        std::vector<std::vector<bool>> res;
-        for(int i = 0; i < mat.size(); ++i) {
-            if(i == arista.u || i == arista.v) {
-                continue;
-            }
-            std::vector<bool> arr;
-            for(int j = 0; j < mat.size(); ++j) {
-                if(j != arista.u && j != arista.v) {
-                    arr.push_back(mat[i][j]);
-                }
-            }
-            res.push_back(arr);
-        }
-
-        int ini = 0;
-        for(int i = 0; i < mat.size(); ++i) {
-            if(i != arista.u && i != arista.v) {
-                res[ini++].push_back(mat[i][arista.u] | mat[i][arista.v]);
-            }
-        }
-
-        res.push_back(std::vector<bool>());
-        int size = res.size() - 1;
-        for(int j = 0; j < mat.size(); ++j) {
-            if(j != arista.u && j != arista.v) {
-                res[size].push_back(mat[arista.u][j] | mat[arista.v][j]);
-            }
-        }
-        res[size].push_back(0);
-        return res;
-    }
-
-    std::string cromático(const std::vector<std::vector<bool>>& mat) {
-        if(no_arista(mat)) {
-            return "x" + std::to_string(mat.size());
-        } else {
-            arista arista = primer_arista(mat);
-            return cromático(borrado(mat, arista)) + "-(" + cromático(contracción(mat, arista)) + ")";
-        }
-    }
-
-    std::vector<int> traductor(int n, std::string fun) {
-        std::vector<int> res(n + 1, 0);
-        int signo = 1;
-        std::vector<int> ant;
-        for(int i = 0; i < fun.size(); ++i) {
-            if(fun[i] == 'x') {
-                std::string num = "";
-                while(std::isdigit(fun[i + 1])) {
-                    num += fun[i + 1];
-                    ++i;
-                }
-                res[std::stoi(num)] += signo;
-            } else if(fun[i] == '-') {
-                ant.push_back(-1);
-                signo *= -1;
-                ++i;
-            } else if(fun[i] == ')') {
-                signo *= ant.back(); ant.pop_back();
-            }
-        }
-        return res;
-    }
-
-    __int128_t evaluación(const std::vector<int>& f, int k) {
-        __int128_t res = 0;
-        for(int i = 0; i < f.size(); ++i) {
-            res += f[i] * potencia(k, i);
-        }
-        return res;
-    }
+    int c;
+    coloración col;
+    std::float128_t media;
 
     public:
-    polinomio(const std::vector<std::vector<bool>>& g) {
+    monteCarlo(std::float128_t z, std::float128_t e, const std::vector<std::vector<bool>>& g, int c) {
+        this->z = z;
+        this->e = e;
         this->g = g;
+        this->c = c;
+    }
+
+    private:
+    void simulación(int bloqueTam, std::atomic<int>& validas) {
+        for (int i = 0; i < bloqueTam; ++i) {
+            std::vector<int> coloración = col.coloraciónAleatoria(g.size(), c);
+            if (col.verificarColoración(g, coloración)) {
+                ++validas;
+            }
+        }
+    }
+
+    std::float128_t montecarlo() {
+        std::float128_t p = 0.5;
+        std::float128_t ep = 1;
+        std::float128_t factor = 1;
+        do {
+            __int128_t n = factor * (potencia(z, 2) * p * (1 - p)) / (potencia(e, 2));
+            std::atomic<int> validas(0);
+            int nhilos = std::thread::hardware_concurrency() - 1;
+            int tam = n / nhilos;
+            int resto = n % nhilos;
+            std::vector<std::thread> hilos;
+            for (int i = 0; i < nhilos; ++i) {
+                int bloqueTam = tam + (i == nhilos - 1 ? resto : 0);
+                hilos.emplace_back(&monteCarlo::simulación, this, bloqueTam, std::ref(validas));
+            }
+            for (auto& hilo : hilos) {
+                hilo.join();
+            }
+            std::float128_t pact = std::float128_t(validas) / std::float128_t(n);
+            if (pact == 0) {
+                if(potencia(c, g.size()) < n) {
+                    return 0;
+                }
+                factor *= 2;
+                continue;
+            }
+            p = pact;
+            ep = sqrtq(p * (1 - p) / n);
+            if (ep > e) {
+                factor *= (1 + (ep - e) / e);
+            }
+        } while (ep > e);
+        return p * potencia(c, g.size());
+    }
+
+    std::vector<std::float128_t> mediamc() {
+        media = montecarlo();
+        std::float128_t cabs, crel;
+        int i = 1;
+        std::vector<std::float128_t> datos = {media};
+        do {
+            std::float128_t actual = montecarlo();
+            std::float128_t manterior = media / std::float128_t(i++);
+            media += actual;
+            datos.push_back(actual);
+            cabs = absoluto(media / std::float128_t(i) - manterior);
+            crel = cabs / manterior;
+        } while(cabs > e && crel > e);
+        media /= std::float128_t(i);
+        return datos;
     }
     
-    void creaPolinomio() {
-        sPolinomio = cromático(g);
-    }
+    public:
+    struct resultado {
+        std::pair<std::float128_t, std::float128_t> intervalo;
+        std::float128_t media;
+    };
 
-    void traducePolinomio() {
-        fun = traductor(g.size(), sPolinomio);
-    }
-
-    __int128_t P(int k) {
-        return evaluación(fun, k);
+    resultado intervalo() {
+        estadística est(z, e, std::float128_t(g.size()));
+        std::vector<std::float128_t> d = mediamc();
+        return {est.intervalo(d, media), media};
     }
 };
